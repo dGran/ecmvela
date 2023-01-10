3a957797e79c7558587253f27c8d3d558facabad
dgrandev <dgranh@gmail.com>
2023-01-10 23:36:57 +0100

    [IMP] home: add legal pages and blank pages appointments, works and contact [IMP] layout: improvement in header, footer and base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,40 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', []);
     }
+
+    #[Route('/citas', name: 'appointments')]
+    public function appointments(): Response
+    {
+        return $this->render('home/appointments/index.html.twig', []);
+    }
+
+    #[Route('/contacto', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact/index.html.twig', []);
+    }
+
+    #[Route('/trabajos', name: 'works')]
+    public function works(): Response
+    {
+        return $this->render('home/works/index.html.twig', []);
+    }
+
+    #[Route('/aviso-legal', name: 'legal_warning')]
+    public function legalWarning(): Response
+    {
+        return $this->render('home/legal/legal-warning.html.twig', []);
+    }
+
+    #[Route('/politica-de-privacidad', name: 'privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('home/legal/privacy-policy.html.twig', []);
+    }
+
+    #[Route('/politica-de-cookies', name: 'cookies_policy')]
+    public function cookiesPolicy(): Response
+    {
+        return $this->render('home/legal/cookies-policy.html.twig', []);
+    }
 }
