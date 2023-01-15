16ca8230c45ed6d89f8eea2719eab4ee8a880515
dgrandev <dgranh@gmail.com>
2023-01-15 10:08:19 +0100

    [IMP] HomeController: path routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,45 +8,51 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'main')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', []);
+        return $this->render('main/index.html.twig', []);
     }
 
     #[Route('/citas', name: 'appointments')]
     public function appointments(): Response
     {
-        return $this->render('home/appointments/index.html.twig', []);
+        return $this->render('main/appointments/index.html.twig', []);
     }
 
     #[Route('/contacto', name: 'contact')]
     public function contact(): Response
     {
-        return $this->render('home/contact/index.html.twig', []);
+        return $this->render('main/contact/index.html.twig', []);
     }
 
     #[Route('/trabajos', name: 'works')]
     public function works(): Response
     {
-        return $this->render('home/works/index.html.twig', []);
+        return $this->render('main/works/index.html.twig', []);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('main/blog/index.html.twig', []);
     }
 
     #[Route('/aviso-legal', name: 'legal_warning')]
     public function legalWarning(): Response
     {
-        return $this->render('home/legal/legal-warning.html.twig', []);
+        return $this->render('main/legal/legal-warning.html.twig', []);
     }
 
     #[Route('/politica-de-privacidad', name: 'privacy_policy')]
     public function privacyPolicy(): Response
     {
-        return $this->render('home/legal/privacy-policy.html.twig', []);
+        return $this->render('main/legal/privacy-policy.html.twig', []);
     }
 
     #[Route('/politica-de-cookies', name: 'cookies_policy')]
     public function cookiesPolicy(): Response
     {
-        return $this->render('home/legal/cookies-policy.html.twig', []);
+        return $this->render('main/legal/cookies-policy.html.twig', []);
     }
 }
