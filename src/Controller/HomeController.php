403d91add2d10d7cb7446f4d6e8d6cb6a6d9baa1
dgrandev <dgranh@gmail.com>
2023-01-22 06:15:02 +0100

    [IMP] HomeController: change home route name [IMP] layout/header: apply home routes, add mobile logo menu icon style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'main')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', []);
