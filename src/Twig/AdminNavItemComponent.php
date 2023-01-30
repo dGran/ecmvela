eab5ed321ca616df7bae4ab6c3dc65aa396423f8
dgran <dgranh@gmail.com>
2023-01-30 19:37:46 +0100

    [IMP] twig/component: add AdminNavItemComponent

---
+++ src/Twig/AdminNavItemComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('adminNavItem')]
+class AdminNavItemComponent
+{
+    public string $path;
+    public string $message;
+    public string $tag = '';
+}
