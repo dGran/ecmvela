717f36673de31a96d85d04c94713aa927bc06963
dgran <dgranh@gmail.com>
2023-10-21 23:09:31 +0200

    [IMP] restore AgendaController

---
+++ src/Controller/Admin/AgendaController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgendaController extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    #[Route('/admin/agenda', name: 'admin_agenda', methods: 'GET')]
+    public function index(Request $request): Response
+    {
+        return $this->render('admin/agenda/index.html.twig', []);
+    }
+}
