d438f2b262f3d2c52a9d085f7630a31fba6b55a2
dgrandev <dgranh@gmail.com>
2023-10-24 21:53:45 +0200

    [DEL] remove unnecessary code

--- src/Controller/Main/LegalController.php
+++ src/Controller/Main/LegalController.php
@@ -6,10 +6,7 @@ namespace App\Controller\Main;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LegalController extends AbstractController
