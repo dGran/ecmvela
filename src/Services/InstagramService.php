638abb8873be78ba2fbb3e5b76489ec0d906b417
dgran <dgranh@gmail.com>
2023-11-21 22:15:51 +0100

    [IMP] refactoring

--- src/Services/InstagramService.php
+++ src/Services/InstagramService.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Client\Instagram\Media\Factory\PublicationRequestFactory;
+use App\Client\Instagram\Media\Factory\MediaRequestFactory;
 use App\Client\Instagram\Media\Factory\PublicationResponseFactory;
 use App\Client\Instagram\Media\Model\MediaResponse;
 use App\Client\Instagram\Media\Model\Publications;
 use App\Client\Instagram\Media\Service\MediaService;
-use App\Helper\Serializer;
+use App\Entity\Client;
+use App\Utils\Serializer;
+use App\Manager\ClientManager;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Contracts\Cache\CacheInterface;
 
@@ -19,14 +21,14 @@ class InstagramService
 
     private const CACHE_KEY_INSTAGRAM_PUBLICATIONS = "instagram_publications";
 
-    private MediaService $mediaService;
-
     private CacheInterface $cache;
 
-    public function __construct(MediaService $mediaService, CacheInterface $cache)
+    private ClientManager $clientManager;
+
+    public function __construct(CacheInterface $cache, ClientManager $clientManager)
     {
-        $this->mediaService = $mediaService;
         $this->cache = $cache;
+        $this->clientManager = $clientManager;
     }
 
     /**
@@ -38,8 +40,12 @@ class InstagramService
         $cachedData = $this->cache->getItem(self::CACHE_KEY_INSTAGRAM_PUBLICATIONS);
 
         if (!$cachedData->isHit()) {
-            $publicationRequest = PublicationRequestFactory::build(self::NUMBER_OF_LAST_PUBLICATIONS);
-            $mediaResponse = $this->mediaService->getMedia($publicationRequest);
+            $client = $this->clientManager->findOneById(Client::TYPE_INSTAGRAM);
+            $mediaService = new MediaService($client);
+
+            $publicationRequest = MediaRequestFactory::build(self::NUMBER_OF_LAST_PUBLICATIONS);
+            $mediaResponse = $mediaService->getMedia($publicationRequest);
+
             $cachedData->set(Serializer::serialize($mediaResponse, Serializer::FORMAT_JSON));
             $cachedData->expiresAfter(new \DateInterval('P1D'));
             $this->cache->save($cachedData);
